27b725426af9045f1c4e228f5d21c0cb312f239b
john199310 <122259771+john199310@users.noreply.github.com>
2023-11-17 05:55:26 +1100

    fix: subscribe mail

--- js/mailchimp.js
+++ js/mailchimp.js
@@ -1,44 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("mc-form").addEventListener("submit", function(event) {
-      event.preventDefault();
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("mc-form").addEventListener("submit", function(event) {
+//       event.preventDefault();
+//       var email = document.createElement('a');
+//       email.href = `mailto:johnwilliam199024@gmail.com?subject=Hello Cryptosi&body ${document.getElementById("mailchimp-fname").value} ,${document.getElementById("mailchimp-email").value}`;  
+//       email.click();
   
-      const apiKey = "";
-      const dc = "us2";
-  
-      const url = `https://us2.api.mailchimp.com/3.0/lists/da5efafe44/`;
-      console.log('here');
-  
-      console.log(document.getElementById("mailchimp-email").value);    
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Authorization": `Basic ${window.btoa(`anystring:${apiKey}`)}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          campaign_defaults: {
-            from_name: document.getElementById("mailchimp-fname").value,
-            from_email: document.getElementById("mailchimp-email").value,
-            subject: "",
-            language: "",
-          },
-        }),
-      })
-        .then(function(response) {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Request failed.");
-          }
-        })
-        .then(function(data) {
-          console.log(data);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-  
-      // You can perform any other actions after the request is sent
-    });
-  });
+//       // You can perform any other actions after the request is sent
+//     });
+//   });
   
